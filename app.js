8a7984a8d6441f88896f9dfacaaee0691ce29107
X2Abdul <87334764+X2Abdul@users.noreply.github.com>
2023-07-10 22:28:11 +0100

    gameboard and player object are done in js

--- app.js
+++ app.js
@@ -1,1 +1,38 @@
-console.log("Working");
+const gameBoard = (() => {
+    let board = new Array(9).fill(undefined);
+    
+    function setContent(sign, index){
+        if(sign === 'X'){
+            board[index] = sign;
+        }
+        if(sign === 'O'){
+            board[index] = sign;
+        }
+    }
+
+    function checkForWinner(playerName){
+        let winnersSign = undefined;
+
+        return winnersSign;
+    }
+    return{
+        board,
+        setContent
+    }
+})();
+
+const player = function(name, sign){
+    name,
+    sign
+    const getName = () => console.log(name);
+    const getSign = () => console.log(sign);
+    return{
+        getName,
+        getSign
+    }
+}
+
+const abdul = new player('Abdul', 'X');
+abdul.getName();
+abdul.getSign();
+console.log(gameBoard.board);
