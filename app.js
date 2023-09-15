890694d2d2b5087b3b02be4bdd18cc3c43b3f242
X2Abdul <87334764+X2Abdul@users.noreply.github.com>
2023-09-15 22:28:59 +0100

    object- done UI - done

--- app.js
+++ app.js
@@ -1,1 +1,43 @@
-console.log("hi")
+const gameBoard = (() => {
+    let board = new Array(9).fill(undefined);
+  
+    function setContent(sign, index) {
+      if (sign === 'X' || sign === 'O') {
+        board[index] = sign;
+      }
+    }
+  
+    function checkForWinner(playerName) {
+      let winnersSign = undefined;
+  
+      return winnersSign;
+    }
+  
+    return {
+      board,
+      setContent,
+    };
+  })();
+  
+  const player = function (name, sign) {
+    const getName = function () {
+      return name;
+    };
+    const getSign = function () {
+      return sign;
+    };
+  
+    return {
+      getName,
+      getSign,
+    };
+  };
+  
+  const abdul = new player('Abdul', 'X');
+  console.log(abdul.getName());
+  console.log(abdul.getSign()); 
+  
+  const board = gameBoard;
+  board.setContent(abdul.getSign(), 2); 
+  console.log(board.board); 
+  
